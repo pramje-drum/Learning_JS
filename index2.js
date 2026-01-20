94ee63f1162a29c14ea172e3fd1dfd10e8f8474f
pramje-drum <pramje@trueigtech.com>
2026-01-20 17:56:09 +0530

    Update

--- index2.js
+++ index2.js
@@ -326,10 +326,48 @@
 // 	signedIn: true,
 
 // 	getUserDetails: function () {
-// 		console.log("Got user details from database");
-// 		console.log(`Username: ${this.username}`);
+// 		// console.log("Got user details from database");
+// 		// console.log(`Username: ${this.username}`);
+//         console.log(this)
 // 	},
 // };
 
 // console.log(user.username);
 // console.log(user.getUserDetails());
+// consosle.log("!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!");
+// console.log(this)
+
+
+
+// function User(username, loginCount, isLoggedIn) {
+//     this.username = username;
+//     this.loginCount = loginCount;
+//     this.isLoggedIn = isLoggedIn;
+
+//     this.greeting = function(){
+//         console.log(`Welcome ${this.username}`)
+//     }
+
+//     return this
+// }
+
+// const userOne = User("Piyush", 12, true)
+// const userTwo = User("Pranay", 13, false)
+// console.log(userOne)
+// console.log(userTwo)
+// userTwo.greeting();
+
+
+// function mult(num){
+//     return num * 5;
+// }
+
+// mult.power = 2
+
+// console.log(mult(5))
+// console.log(mult.power)
+// console.log(mult.prototype)
+
+///////////////////////////////////////////////////////////////////////////////////////////////////
+//constructor functions:
+
